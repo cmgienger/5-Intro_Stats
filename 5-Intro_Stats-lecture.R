9279a0fe35cee2cf120e01917c1ea05b7ff02e51
C.M. Gienger <39389399+cmgienger@users.noreply.github.com>
2023-11-01 10:44:56 -0500

    updated

--- 5-Intro_Stats-lecture.R
+++ 5-Intro_Stats-lecture.R
@@ -68,4 +68,24 @@ ggplot(ozone, aes(x=Garden.location, y=Ozone)) +
   geom_dotplot(binaxis = "y", stackdir = "center", binwidth=2) +
   theme_bw()
 
+#########################################################
+#Supplemental: t-test is just a linear model
+#########################################################
+
+#make up some data
+x = rnorm(10,0.5) #create 10 values from a normal dist. with a mean of 0.5
+y = rnorm(10,-0.5) #create 10 values from a normal dist. with a mean of -0.5
+t.test(x,y)
+
+#do the same analysis from a dataframe instead
+df = data.frame(value=c(x,y), group=as.factor(rep(c('g1','g2'), each=10)))
+t.test(value ~ group, data=df)
+
+#t-test is just a linear model
+t.test(value ~ group, data=df, var.equal=TRUE)
+
+#which is equivalent to
+res = lm(value ~ group, data=df)
+summary(res)
+
 
